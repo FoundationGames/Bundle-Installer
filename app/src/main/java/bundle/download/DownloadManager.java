77b4f0c2ee655c6af1c2ba64940ebf3e193fb2ed
FoundationGames <43485105+FoundationGames@users.noreply.github.com>
2021-04-12 15:05:48 -0700

    Fix multi-downloads downloading everything

--- app/src/main/java/bundle/download/DownloadManager.java
+++ app/src/main/java/bundle/download/DownloadManager.java
@@ -22,9 +22,12 @@ public enum DownloadManager {;
     public static List<DownloadException> downloadFilesTo(Path path, DownloadConfig config) {
         List<DownloadException> errors = new ArrayList<>();
         for (ImmutableList<AbstractDownload> dlList : config.downloads.values()) {
+            boolean success = false;
             for (AbstractDownload download : dlList) {
+                if (success) continue;
                 try {
                     download.downloadTo(path);
+                    success = true;
                 } catch (DownloadException e) {
                     errors.add(e);
                 }
